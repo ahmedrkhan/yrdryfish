c4797065a70f6dadac0aa60b8a097c4a09c21132
Ahmed Raza Khan <ahmedrk128@gmail.com>
2026-02-15 09:23:24 +0530

    final deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/yrdryfish/',  // << must match your repo name
   plugins: [react()],
-   base: "/yrdryfish/",   // VERY IMPORTANT
-})
+});
+// chaged
