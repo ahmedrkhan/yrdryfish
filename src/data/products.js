bcec424addc0be72a267eea7502c65dc30ce5145
Ahmed Raza Khan <ahmedrk128@gmail.com>
2026-02-15 11:01:26 +0530

    final deploy fix

--- src/data/products.js
+++ src/data/products.js
@@ -1,96 +1,96 @@
 const products = [
   {
     id: 1,
-    name: "Premium Dry Prawns",
-    price: 850,
-    category: "Prawns",
-    stock: 25,
+    name: "Arnala Bomil",
+    price: 700,
+    category: "Bomil",
+    stock: 2500,
     rating: 4.8,
     description:
-      "High-quality dry prawns, handpicked and naturally sun-dried for intense flavor. Perfect for curries or snacks.",
+      "High-quality dry Bomil, handpicked and naturally sun-dried for intense flavor. Perfect for curries or snacks.",
     images: [
-      `${import.meta.env.BASE_URL}images/mandeli.png`,
-      `${import.meta.env.BASE_URL}images/yellow-jawala.png`
+      `${import.meta.env.BASE_URL}images/harnala2.png`,
+      `${import.meta.env.BASE_URL}images/harnal-bomil.png`
     ],
-    video: `${import.meta.env.BASE_URL}videos/jawla.mp4`
+    video: `${import.meta.env.BASE_URL}videos/harnalabomil.mp4`
   },
   {
     id: 2,
     name: "Gujarat Bomil",
-    price: 400,
-    category: "Small Fish",
-    stock: 40,
+    price: 420,
+    category: "Bomil",
+    stock: 4000,
     rating: 4.5,
     description:
       "Fresh small fish from Gujarat, ideal for light frying and traditional recipes.",
     images: [
-      `${import.meta.env.BASE_URL}images/mandeli.png`,
-      `${import.meta.env.BASE_URL}images/yellow-jawala.png`
+      `${import.meta.env.BASE_URL}images/gujaarta-bomil.png`,
+      `${import.meta.env.BASE_URL}images/gujaratbomil2.webp`
     ],
-    video: `${import.meta.env.BASE_URL}videos/kardi.mp4`
+    video: `${import.meta.env.BASE_URL}videos/harnalabomil.mp4`
   },
   {
     id: 3,
-    name: "Dry Mandeli",
-    price: 1200,
-    category: "Large Fish",
-    stock: 10,
+    name: "Dry Jawla",
+    price: 250,
+    category: "small fish",
+    stock: 10000,
     rating: 4.7,
     description:
-      "Premium large fish dried naturally, rich in protein and perfect for gourmet dishes.",
+      "Premium small fish dried naturally, rich in protein and perfect for gourmet dishes.",
     images: [
-      `${import.meta.env.BASE_URL}images/mandeli.png`,
+      `${import.meta.env.BASE_URL}images/white-jawala.png`,
       `${import.meta.env.BASE_URL}images/yellow-jawala.png`
     ],
-    video: `${import.meta.env.BASE_URL}videos/wakati.mp4`
+    video: `${import.meta.env.BASE_URL}videos/jawla.mp4`
 
   },
   {
     id: 4,
-    name: "Yellow Jawala",
-    price: 900,
+    name: "Kardi (small prawns)",
+    price: 500,
     category: "Prawns",
-    stock: 15,
+    stock: 1500,
     rating: 4.6,
     description:
-      "Sun-dried yellow jawala prawns with a rich taste and aromatic flavor. Excellent for special seafood recipes.",
+      "Sun-dried  prawns with a rich taste and aromatic flavor. Excellent for special seafood recipes.",
     images: [
-      `${import.meta.env.BASE_URL}images/mandeli.png`,
-      `${import.meta.env.BASE_URL}images/yellow-jawala.png`
+      `${import.meta.env.BASE_URL}images/kardi.png`,
+      `${import.meta.env.BASE_URL}images/kardi2.png`
     ]
     ,
     video: `${import.meta.env.BASE_URL}videos/kardi.mp4`
   },
   {
     id: 5,
-    name: "Small Gujarat Bomil",
-    price: 450,
-    category: "Small Fish",
-    stock: 30,
+    name: "Bangda dry Fish",
+    price: "250 - 12pieces" ,
+    category: "Fish",
+    stock: 3000,
     rating: 4.3,
     description:
-      "Tender small fish from Gujarat, lightly salted and sun-dried for freshness.",
+      "Tender small fish from oceans, lightly salted and sun-dried for freshness.",
     images: [
-      `${import.meta.env.BASE_URL}images/mandeli.png`,
-      `${import.meta.env.BASE_URL}images/yellow-jawala.png`
+      `${import.meta.env.BASE_URL}images/bangda.jpg`,
+      `${import.meta.env.BASE_URL}images/bangda2.jpg`
     ],
-    video: `${import.meta.env.BASE_URL}videossurmai-piece.mp4`
+    video: `${import.meta.env.BASE_URL}bangda.mp4`
   },
   {
     id: 6,
-    name: " Bangda",
+    name: " Mandeli",
     price: 400,
     category: "Small Fish",
-    stock: 40,
+    stock: 4000,
     rating: 4.5,
     description:
-      "Fresh small fish from Gujarat, ideal for light frying and traditional recipes.",
+      "Fresh small fish from Oceans, ideal for light frying and traditional recipes.",
     images: [
       `${import.meta.env.BASE_URL}images/mandeli.png`,
-      `${import.meta.env.BASE_URL}images/yellow-jawala.png`
+      `${import.meta.env.BASE_URL}images/mandeli2.png`
     ]
     ,
-    video: `${import.meta.env.BASE_URL}videos/bangda.mp4`
+    video: `${import.meta.env.BASE_URL}videos/wakati.mp4`
   },
 ];
 
